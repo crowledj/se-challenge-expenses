dedc09177344cac206219757dbb7a1510a0ec18b
crowledj <crowledj@tcd.ie>
2018-02-06 23:37:24 -0500

    attempt at filling db table

--- wave_test_davidCrowley.py
+++ wave_test_davidCrowley.py
@@ -8,6 +8,7 @@ import re
 from shutil import copyfile
 import matplotlib.pyplot as plt
 import sqlalchemy,sqlite3
+import numpy as np
 
 
 ## Initialize variables with 'wanted' type
@@ -25,79 +26,64 @@ category_vect=[]
 employee_name_vect=[]
 employee_address_vect=[]
 expense_description_vect=[]
-#pre-tax_amount_vect=[]
+pre_tax_amounts=[]
 tax_name_vect=[]
-#tax_amount=[]
+tax_amounts=[]
 
 
 filename=sys.argv[1];
 #folder=sys.argv[2];
 ind=0;
 
-#filenames1 = os.listdir(folder) 
 
 ## grab headers first :
 
-
-
-
-with open(filename) as csvfile:       
-
-    for indx,row in enumerate(csvfile):  
+with open(filename) as f:
+    reader = csv.reader(f)
+    for record in reader:
+        print(record)   
         
-        record=row.split(',') 
-        #ind+=1
-        
-        if indx == 0: 
-            continue
-        
-        if indx >= 1:
-            #prev_stu_ID= record_prev[3][:-1]
-            #prev_Q_ID= record_prev[0]
-            
-            dates_vect.append(record[0][:])
-            category_vect.append(record[1][:])             
-            employee_name_vect.append(record[2][:])
-            employee_address_vect.append(record[3][:])
-            expense_description_vect.append(record[4][:])
-            tax_name_vect.append(record[5][:])
-            
-            #break 
+        dates_vect.append(record[0][:])
+        category_vect.append(record[1][:])             
+        employee_name_vect.append(record[2][:])
+        employee_address_vect.append(record[3][:])
+        expense_description_vect.append(record[4][:])
+        pre_tax_amounts.append(record[5][:])
+        tax_name_vect.append(record[6][:])         
+        tax_amounts.append(record[7][:])
 
-        #break
 
-#indx=0    
+# initialize a dictionary to store the parsaed .csv data  - this is a good data struct to use as it can store all data types, 
+# it will ensure keys used to insert into the database are also unique ( rephrase ?) and ...  
 
+## is an intermediate data struct storage even necessary prior to insertion into the dB ?
 
 staff_data={}
-for i,val in enumerate(tax_name_vect):
-    key=val+employee_name_vect[i]
-    print("key " + str(i) + " = " + str(key) + "/n ")     
-    staff_data[key]=(dates_vect[i],category_vect[i],employee_name_vect[i],employee_address_vect[i],expense_description_vect[i])
+keys=[]
+for i,val in enumerate(employee_name_vect):
+    key= val+dates_vect[i] + employee_address_vect[i]
+    #keys.append(key)
+    print("key " + str(i) + " = " + str(key))     
+    staff_data[key]=(dates_vect[i],category_vect[i],employee_name_vect[i],employee_address_vect[i],expense_description_vect[i],pre_tax_amounts[i],tax_name_vect[i],tax_amounts[i])
 
 
 ## upload data to dB :
-
-
-
-#SQL create table "Expenses"
- 
- 
-#SQL INSERT datra into  "Expenses"  WHERE ?
- 
- 
 connection = sqlite3.connect("company.db")
 cursor = connection.cursor()
-sql_cmd= """ CREATE TABLE employee_2 (  tax_name_vect INTEGER PRIMARY KEY, date DATE, category VARCHAR(30),employee_name VARCHAR(30),address VARCHAR(30),expense_description_vect INTEGER); """
+sql_cmd= """ CREATE TABLE employee_2 (  keyz VARCHAR(50) PRIMARY KEY, date DATE, category VARCHAR(30),employee_name VARCHAR(30),address VARCHAR(30),expense_description_vect INTEGER,pre_tax_amounts INTEGER,tax_name_vect VARCHAR(30),tax_amounts INTEGER); """
 cursor.execute(sql_cmd)
 
 
 #staff_data = [ ("William", "Shakespeare", "m", "1961-10-25"), ("Frank", "Schiller", "m", "1955-08-17"), ("Jane", "Wall", "f", "1989-03-14") ]
 for key in staff_data:
-    format_str = """INSERT INTO employee_2 ( date, category, employee_name, address,expense_description_vect) VALUES ("{date}", "{category}", "{employee_name}", "{address}","{expense_description_vect}");"""   
-    sql_command = format_str.format(date=staff_data[key][0], category=staff_data[key][1], employee_name=staff_data[key][2], address = staff_data[key][3] , expense_description_vect=staff_data[key][4])
+    format_str = """ INSERT INTO employee_2 (keyz, date, category, employee_name, address,expense_description_vect,pre_tax_amounts,tax_name_vect,tax_amounts) VALUES ( "{keyz}",{date}", "{category}", "{employee_name}", "{address}","{expense_description_vect}","{pre_tax_amounts}","{tax_name_vect}","{tax_amounts}");"""   
+    sql_command = format_str.format(keyz=key,date=staff_data[key][0], category=staff_data[key][1], employee_name=staff_data[key][2], address = staff_data[key][3] , expense_description_vect=staff_data[key][4],pre_tax_amounts=staff_data[key][5],tax_name_vect=staff_data[key][6],tax_amounts=staff_data[key][7])
     cursor.execute(sql_command)
     
+    
+    
+cursor.execute("SELECT * FROM employee_2;")
+print(cursor.fetchall())    
     
     
 
